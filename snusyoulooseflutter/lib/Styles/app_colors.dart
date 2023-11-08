494bae888604e744da252b051b388afd765a51b1
Frans Billqvist <frans.billquist@gmail.com>
2023-11-08 18:12:11 +0100

    Added Styles W/ example colorpalettes

---
+++ snusyoulooseflutter/lib/Styles/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF0D47A1);
+  static const example1 = Color(0xFF5D737E);
+  static const example2 = Color(0xFF64B6AC);
+  static const example3 = Color(0xFFC0FDFB);
+  static const example4 = Color(0xFFDAFFEF);
+  static const example5 = Color(0xFFFCFFFD);
+}
