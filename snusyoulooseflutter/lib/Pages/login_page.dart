c212ead28eb87ce39fbce7f1d14af496331a747c
Frans Billqvist <frans.billquist@gmail.com>
2023-11-10 20:23:05 +0100

    Made boiler login page

--- snusyoulooseflutter/lib/Pages/login_page.dart
+++ snusyoulooseflutter/lib/Pages/login_page.dart
@@ -5,6 +5,65 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Column(children: [
+            Expanded(
+              flex: 2,
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/login_background.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32),
+                child: Column(
+                  children: [
+                    SizedBox(height: 32),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Login'),
+                    ),
+                    SizedBox(height: 16),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text('Forgot password?'),
+                    ),
+                    SizedBox(height: 16),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text('Create account'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
   }
 }
