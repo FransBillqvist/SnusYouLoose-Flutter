41bf466725afffb8ff8f380908e94b0c972226a6
Frans Billqvist <frans.billquist@gmail.com>
2023-11-12 03:17:27 +0100

    A: base icons

--- snusyoulooseflutter/lib/Config/app_media.dart
+++ snusyoulooseflutter/lib/Config/app_media.dart
@@ -4,4 +4,13 @@ class AppImages {
   static const logo_background = '${_img}logo_background.png';
 }
 
-class AppIcons {}
+class AppIcons {
+  static const _icn = 'Assets/Icons/';
+
+  static const add = '${_icn}add.svg';
+  static const favorite = '${_icn}favorite.svg';
+  static const home = '${_icn}home.svg';
+  static const location = '${_icn}location.svg';
+  static const messages = '${_icn}messages.svg';
+  static const user = '${_icn}user.svg';
+}
