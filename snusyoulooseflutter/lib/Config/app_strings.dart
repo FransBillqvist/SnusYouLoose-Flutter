aa20d33428e41b76c36276822ba5b22e19fc32f3
Frans Billqvist <frans.billquist@gmail.com>
2023-11-14 07:28:33 +0100

    A: New class Appstrings to have all text as Varibles

---
+++ snusyoulooseflutter/lib/Config/app_strings.dart
@@ -0,0 +1,11 @@
+class AppStrings {
+  static const String appName = 'SnusYouLoose';
+  static const String username = 'Username';
+  static const String password = 'Password';
+  static const String login = 'Login';
+  static const String register = 'Register';
+  static const String forgotPassword = 'Forgot Password?';
+  static const String email = 'Email';
+  static const String loginWithGoogle = 'Login with Google';
+  static const String loginWithFacebook = 'Login with Facebook';
+}
