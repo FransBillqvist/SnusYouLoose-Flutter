e4697335bf0d82bee1834ba300b516fe08f2b00e
Frans Billqvist <frans.billquist@gmail.com>
2023-11-14 07:38:45 +0100

    Add text field hint color and text on focus color to AppColors

--- snusyoulooseflutter/lib/Styles/app_colors.dart
+++ snusyoulooseflutter/lib/Styles/app_colors.dart
@@ -7,4 +7,6 @@ class AppColors {
   static const example3 = Color(0xFFC0FDFB);
   static const example4 = Color(0xFFDAFFEF);
   static const example5 = Color(0xFFFCFFFD);
+  static final textFieldHintColor = const Color(0xffD9D9D9).withOpacity(0.6);
+  static const textOnFocus = Color.fromARGB(178, 44, 15, 232);
 }
