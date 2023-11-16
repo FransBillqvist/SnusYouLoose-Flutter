18e83f73eb5b9c14c7b51f05837fbdb7f3b15626
Frans Billqvist <frans.billquist@gmail.com>
2023-11-16 08:17:34 +0100

    Add animated background and text color to login page

--- snusyoulooseflutter/lib/Pages/login_page.dart
+++ snusyoulooseflutter/lib/Pages/login_page.dart
@@ -19,25 +19,27 @@ class _LoginPageState extends State<LoginPage>
   final PWController = TextEditingController();
 
   late final AnimationController _controller;
-  late final Animation<Color?> _colorAnimation;
+  late final Animation<Color?> _bgColorAnimation;
+  late final Animation<Color?> _textColorAnimation;
 
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(
+      lowerBound: 0.6,
       duration: const Duration(seconds: 5),
       vsync: this,
     )..repeat(reverse: true);
 
-    _colorAnimation = ColorTween(
-      begin: Color(0xFF2E5B69),
-      end: Color(0xFF001A27),
+    _bgColorAnimation = ColorTween(
+      begin: AppColors.loginLightBackgroundColor,
+      end: Color.fromARGB(255, 19, 55, 84),
     ).animate(_controller);
 
-    // You can listen to the controller and set the state to update the color in real time
-    _controller.addListener(() {
-      setState(() {});
-    });
+    _textColorAnimation = ColorTween(
+      begin: Color.fromARGB(255, 255, 255, 255),
+      end: Color.fromARGB(255, 195, 227, 215),
+    ).animate(_controller);
   }
 
   @override
@@ -50,32 +52,44 @@ class _LoginPageState extends State<LoginPage>
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: _colorAnimation.value, // Animated background color
-      resizeToAvoidBottomInset: false,
-      body: SingleChildScrollView(
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height,
-          child: Column(children: [
-            SizedBox(height: 100),
-            Text(AppStrings.appName, textScaleFactor: 2),
-            SizedBox(height: 40),
-            AppTextField(
-              controllerName: UNController,
-              labelText: AppStrings.username,
-              textAlignment: "center",
-              keyboardType: "number",
+    return AnimatedBuilder(
+      animation: Listenable.merge([_bgColorAnimation, _textColorAnimation]),
+      builder: (context, child) {
+        return Scaffold(
+          backgroundColor: _bgColorAnimation.value,
+          resizeToAvoidBottomInset: false,
+          body: SingleChildScrollView(
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                children: [
+                  SizedBox(height: 100),
+                  Text(
+                    AppStrings.appName,
+                    textScaleFactor: 2,
+                    style: TextStyle(color: _textColorAnimation.value),
+                  ),
+                  SizedBox(height: 40),
+                  AppTextField(
+                    controllerName: UNController,
+                    labelText: AppStrings.username,
+                    textAlignment: "center",
+                    keyboardType: "number",
+                  ),
+                  SizedBox(height: 16),
+                  AppTextField(
+                    controllerName: PWController,
+                    labelText: AppStrings.password,
+                    textAlignment: "center",
+                    obscureText: true,
+                  ),
+                  // Other widgets...
+                ],
+              ),
             ),
-            SizedBox(height: 16),
-            AppTextField(
-              controllerName: PWController,
-              labelText: AppStrings.password,
-              textAlignment: "center", // Assuming you want to hide the password
-            ),
-            // Other widgets...
-          ]),
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
