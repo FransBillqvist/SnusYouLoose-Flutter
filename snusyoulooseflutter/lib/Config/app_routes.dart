5ec52f8bafb590abb88e5be1f6265ef67c8c6b5c
Frans Billqvist <frans.billquist@gmail.com>
2023-11-20 05:12:17 +0100

    Add new route: home page

--- snusyoulooseflutter/lib/Config/app_routes.dart
+++ snusyoulooseflutter/lib/Config/app_routes.dart
@@ -1,9 +1,12 @@
+import '../Pages/home_page.dart';
 import '../Pages/login_page.dart';
 
 class AppRoutes {
   static final pages = {
     login: (context) => LoginPage(),
+    home: (context) => HomePage(),
   };
 
   static const String login = '/';
+  static const String home = '/home';
 }
