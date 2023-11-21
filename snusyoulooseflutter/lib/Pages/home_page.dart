7f8c16910956fe0f7822d3e0d2f1bb8510724f7c
Frans Billqvist <frans.billquist@gmail.com>
2023-11-21 20:30:01 +0100

    Update home_page.dart with new layout and navigation

--- snusyoulooseflutter/lib/Pages/home_page.dart
+++ snusyoulooseflutter/lib/Pages/home_page.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:snusyoulooseflutter/Components/app_iconbutton.dart';
+import 'package:snusyoulooseflutter/Styles/app_colors.dart';
+
+import '../Config/app_media.dart';
+import '../Config/app_routes.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Center(
-      child: Text('Home Page'),
-    ));
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text(
+              'Home Page',
+              style: TextStyle(color: AppColors.primary),
+            ),
+            SizedBox(height: 200),
+            AppIconButton(
+                onPressed: () =>
+                    Navigator.of(context).pushNamed(AppRoutes.login),
+                icon: AppIcons.home)
+          ],
+        ),
+      ),
+    );
   }
 }
