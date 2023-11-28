d3d648c39d0bcff8dc4e5d08f9fb5b7cc42e95ac
Frans Billqvist <frans.billquist@gmail.com>
2023-11-28 05:31:30 +0100

    Add Snuff model class

---
+++ snusyoulooseflutter/lib/Model/Snuff.dart
@@ -0,0 +1,11 @@
+class Snuff {
+  final String Brand;
+  final String Type;
+  final int Price;
+  final int DefaultAmount;
+  final String ImageUrl;
+  final int Size;
+
+  Snuff(this.Brand, this.Type, this.Price, this.DefaultAmount, this.ImageUrl,
+      this.Size);
+}
