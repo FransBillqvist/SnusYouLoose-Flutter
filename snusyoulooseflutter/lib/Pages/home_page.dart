cca6e5e1f3b813ddd65c5bbd37a9fc617e56a63d
Frans Billqvist <frans.billquist@gmail.com>
2023-11-28 03:42:15 +0100

    Add fetchUserInfo function to HomePage

--- snusyoulooseflutter/lib/Pages/home_page.dart
+++ snusyoulooseflutter/lib/Pages/home_page.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:snusyoulooseflutter/Components/app_iconbutton.dart';
-import 'package:snusyoulooseflutter/Styles/app_colors.dart';
+import 'package:http/http.dart' as http;
 
+import '../Components/app_iconbutton.dart';
+import '../Styles/app_colors.dart';
 import '../Config/app_media.dart';
 import '../Config/app_routes.dart';
+import '../Config/app_urls.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,5 +34,20 @@ class HomePage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  fetchUserInfo(String userId) async {
+    // Make API call with the authUserId
+    final response = await http.post(Uri.parse(AppUrls.fetchUserGatewat),
+        body: {'authUserId': userId});
+
+    // Handle the response
+    if (response.statusCode == 200) {
+      // Successful login
+      print('Successfully fetched user info');
+    } else {
+      // Failed login
+      print('Failed to fetch user info');
+    }
   }
 }
