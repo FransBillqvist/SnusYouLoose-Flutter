f2b822b104a0e163c580512ee1133a3605a377d4
Frans Billqvist <frans.billquist@gmail.com>
2023-12-02 06:04:50 +0100

    Add BoughtSnuff class to Snuff.dart

--- snusyoulooseflutter/lib/Model/Snuff.dart
+++ snusyoulooseflutter/lib/Model/Snuff.dart
@@ -13,3 +13,15 @@ class Snuff {
   Snuff(this.id, this.Brand, this.Type, this.Price, this.DefaultAmount,
       this.ImageUrl, this.Size, this.selected);
 }
+
+class BoughtSnuff {
+  Snuff snuff;
+  DateTime BoughtDate;
+  DateTime FirstDoseDate;
+  int AmountLeft;
+  bool InUse;
+  bool Empty;
+
+  BoughtSnuff(this.snuff, this.BoughtDate, this.FirstDoseDate, this.AmountLeft,
+      this.InUse, this.Empty);
+}
