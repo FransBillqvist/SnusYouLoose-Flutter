da2b507a2ea63cc452f164966d48ae40bef21ee5
Frans Billqvist <frans.billquist@gmail.com>
2023-12-09 07:05:38 +0100

    Refactor loginRequest and fetchUserDetails functions

--- snusyoulooseflutter/lib/Services/app_services.dart
+++ snusyoulooseflutter/lib/Services/app_services.dart
@@ -7,27 +7,35 @@ import '/Model/LoginResponse.dart';
 import '/Config/app_urls.dart';
 
 Future<LoginResponse> loginRequest(String email, String password) async {
-  final url = Uri.parse(AppUrls.loginGateway);
   final body = {
     'Email': email,
     'Password': password,
   };
+  print(body);
+  try {
+    final response = await http.post(Uri.parse(AppUrls.loginGateway),
+        headers: {'Content-Type': 'application/json'}, body: jsonEncode(body));
 
-  final response = await http.post(url,
-      headers: {'Content-Type': 'application/json'}, body: body);
-
-  if (response.statusCode == 200) {
-    return LoginResponse.fromJson(jsonDecode(response.body));
-  } else {
+    if (response.statusCode == 200) {
+      print("this is the response: ${response.body} ");
+      return LoginResponse.fromJson(jsonDecode(response.body));
+    } else {
+      print("FAILED TO LOGIN loginRequest");
+      throw Exception('Failed to login');
+    }
+  } catch (e) {
+    print("FAILED TO LOGIN loginRequest");
     throw Exception('Failed to login');
   }
 }
 
 Future<User> fetchUserDetails(String userId) async {
-  final url = Uri.parse(AppUrls.fetchUserGateway + userId);
-  final response = await http.get(url);
-
+  final response = await http.get(Uri.parse(AppUrls.fetchUserGateway + userId),
+      headers: {'Content-Type': 'application/json'});
+  print("This is the FetchUserDetails response: ${response.body} ");
   if (response.statusCode == 200) {
+    final newUser = User.fromJson(jsonDecode(response.body));
+    print("YESSS SIIIR");
     return User.fromJson(jsonDecode(response.body));
   } else {
     throw Exception('Failed to fetch user details');
