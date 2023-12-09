f60d3ec402784bb8fba654d06a9c9cd2e1b66ac8
Frans Billqvist <frans.billquist@gmail.com>
2023-12-09 02:26:06 +0100

    Refactor Redux reducer and update state management

--- snusyoulooseflutter/lib/Redux/reducer.dart
+++ snusyoulooseflutter/lib/Redux/reducer.dart
@@ -1,18 +1,70 @@
-import 'package:provider/provider.dart';
-
+import '../Model/Habit.dart';
+import '../Model/Snuff.dart';
+import '../Model/User.dart';
 import 'actions.dart';
 import 'app_state.dart';
 
-AppState updatedSnuffReducer(AppState state, dynamic action) {
-  if (action is UpdateSnuffAction) {
-    return AppState(
-      snuffs: state.snuffs
-          .map((snuff) => snuff.Type == action.updatedSnuff.Type
-              ? action.updatedSnuff
-              : snuff)
-          .toList(),
-    );
-  }
-
-  return state;
+AppState appReducer(AppState state, action) {
+  return AppState(
+    snuffs: snuffReducer(state.snuffs, action),
+    habit: habitReducer(state.habit, action),
+    user: userReducer(state.user, action),
+  );
 }
+
+List<Snuff> snuffReducer(List<Snuff> snuffs, action) {
+  if (action is UpdateSnuffAction) {
+    return snuffs
+        .map((snuff) => snuff.Type == action.updatedSnuff.Type
+            ? action.updatedSnuff
+            : snuff)
+        .toList();
+  }
+  return snuffs; // Returnera den oförändrade listan om ingen action matchar
+}
+
+Habit? habitReducer(Habit? habit, action) {
+  if (action is CreateHabitAction) {
+    // Logik för att skapa eller uppdatera en habit
+  }
+  return habit; // Returnera den oförändrade habit om ingen action matchar
+}
+
+User? userReducer(User? user, action) {
+  if (action is SignInUserAction) {
+    // Logik för att hantera användarinloggning
+  }
+  return user; // Returnera den oförändrade användaren om ingen action matchar
+}
+
+
+
+// import 'package:provider/provider.dart';
+
+// import 'actions.dart';
+// import 'app_state.dart';
+
+
+
+
+// AppState appReducer(AppState state, dynamic action) {
+//   return AppState(
+//     snuffs: updatedSnuffReducer(state.snuffs, action),
+//     habit: updatedHabitReducer(state.habit, action),
+//     user: updatedUserReducer(state.user, action),
+//   )
+// }
+
+// AppState updatedSnuffReducer(AppState state, dynamic action) {
+//   if (action is UpdateSnuffAction) {
+//     return AppState(
+//       snuffs: state.snuffs
+//           .map((snuff) => snuff.Type == action.updatedSnuff.Type
+//               ? action.updatedSnuff
+//               : snuff)
+//           .toList(),
+//     );
+//   }
+
+//   return state;
+// }
