dd29cc04167f0859b79ca33c66e560f961bb6b9e
Frans Billqvist <frans.billquist@gmail.com>
2023-12-09 02:25:46 +0100

    Add Habit and User models to AppState

--- snusyoulooseflutter/lib/Redux/app_state.dart
+++ snusyoulooseflutter/lib/Redux/app_state.dart
@@ -1,7 +1,11 @@
+import '../Model/Habit.dart';
 import '../Model/Snuff.dart';
+import '../Model/User.dart';
 
 class AppState {
   List<Snuff> snuffs;
+  Habit? habit;
+  User? user;
 
-  AppState({this.snuffs = const []});
+  AppState({this.snuffs = const [], this.habit, this.user});
 }
