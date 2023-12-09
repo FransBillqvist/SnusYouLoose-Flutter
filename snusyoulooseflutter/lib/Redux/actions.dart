920a5eb30894f5c2a2c4d4cda0eeb3c72aafe18e
Frans Billqvist <frans.billquist@gmail.com>
2023-12-09 02:25:39 +0100

    Add actions for updating snuff, creating habit, and signing in user

--- snusyoulooseflutter/lib/Redux/actions.dart
+++ snusyoulooseflutter/lib/Redux/actions.dart
@@ -1,7 +1,21 @@
+import '../Model/Habit.dart';
 import '../Model/Snuff.dart';
+import '../Model/User.dart';
 
 class UpdateSnuffAction {
   final Snuff updatedSnuff;
 
   UpdateSnuffAction(this.updatedSnuff);
+}
+
+class CreateHabitAction {
+  final Habit habit;
+
+  CreateHabitAction(this.habit);
+}
+
+class SignInUserAction {
+  final User user;
+
+  SignInUserAction(this.user);
 }
