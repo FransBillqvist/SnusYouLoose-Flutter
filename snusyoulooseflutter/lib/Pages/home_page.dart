3122533a416316c22410bfae7a4c4f9980d985d5
Frans Billqvist <frans.billquist@gmail.com>
2023-12-09 02:25:25 +0100

    Add imports and fetch user info and habit

--- snusyoulooseflutter/lib/Pages/home_page.dart
+++ snusyoulooseflutter/lib/Pages/home_page.dart
@@ -1,9 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:http/http.dart' as http;
+import 'package:snusyoulooseflutter/Model/User.dart';
 import 'package:snusyoulooseflutter/Redux/actions.dart';
 
 import '../Components/app_iconbutton.dart';
+import '../Model/Habit.dart';
 import '../Model/Snuff.dart';
 import '../Redux/app_state.dart';
 import '../Styles/app_colors.dart';
@@ -20,24 +24,36 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late Future<User> futureUser;
+  late Future<Habit> futureHabit;
+
+  @override
+  void initState() {
+    super.initState();
+    futureUser = fetchUserInfo('userId');
+    futureHabit = fetchUserHabit('userId');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: StoreConnector<AppState, List<Snuff>>(
           converter: (store) => store.state.snuffs,
-          builder: (context, List<Snuff> stateSnuff) =>
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            SizedBox(
-              height: 164,
-              width: MediaQuery.of(context).size.width,
-            ),
-            Text(
-              "SNUS",
-              style: TextStyle(color: AppColors.primary, fontSize: 48),
-            ),
-            ...stateSnuff
-                .map((snuff) => SnuffWidget(
+          builder: (context, List<Snuff> stateSnuff) => Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 164,
+                width: MediaQuery.of(context).size.width,
+              ),
+              Text(
+                "SNUS",
+                style: TextStyle(color: AppColors.primary, fontSize: 48),
+              ),
+              ...stateSnuff
+                  .map(
+                    (snuff) => SnuffWidget(
                       snuff: snuff,
                       onChanged: (onChange) {
                         snuff.selected = !snuff.selected;
@@ -45,26 +61,50 @@ class _HomePageState extends State<HomePage> {
                             .dispatch(UpdateSnuffAction(snuff));
                       },
                       key: ValueKey(snuff.id),
-                    ))
-                .toList(),
-          ]),
+                    ),
+                  )
+                  .toList(),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  fetchUserInfo(String userId) async {
+  Future<User> fetchUserInfo(String userId) async {
     // Make API call with the authUserId
-    final response = await http.post(Uri.parse(AppUrls.fetchUserGatewat),
+    final response = await http.post(Uri.parse(AppUrls.fetchUserGateway),
         body: {'authUserId': userId});
 
     // Handle the response
     if (response.statusCode == 200) {
       // Successful login
+      final responseData = jsonDecode(response.body);
+      final user = User.fromJson(responseData);
       print('Successfully fetched user info');
+      return user;
+    } else {
+      print('Failed to fetch user info');
+      throw Exception('Failed to fetch user info');
+    }
+  }
+
+  Future<Habit> fetchUserHabit(String userId) async {
+    // Make API call with the authUserId
+    final response = await http.post(Uri.parse(AppUrls.fetchHabitGateway),
+        body: {'authUserId': userId});
+
+    // Handle the response
+    if (response.statusCode == 200) {
+      final responseData = jsonDecode(response.body);
+      final habit = Habit.fromJson(responseData);
+      print('SUCCESFULLY fetched user habit');
+      return habit;
     } else {
       // Failed login
-      print('Failed to fetch user info');
+      print('FAILED to fetch user habit');
+      throw Exception('Failed to fetch user habit');
+      // return Habit('','',-999,'','',-999,DateTime.parse('1970-01-12'),DateTime.parse('1970-01-12'),'','');
     }
   }
 }
