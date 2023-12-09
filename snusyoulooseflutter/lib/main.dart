65f4f54e5c442e174f2d984f7e671ace818111f2
Frans Billqvist <frans.billquist@gmail.com>
2023-12-09 02:25:03 +0100

    Update snuff list in main.dart

--- snusyoulooseflutter/lib/main.dart
+++ snusyoulooseflutter/lib/main.dart
@@ -1,39 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
-import 'package:snusyoulooseflutter/Config/app_routes.dart';
-import 'package:snusyoulooseflutter/Redux/reducer.dart';
-import 'package:snusyoulooseflutter/Styles/app_colors.dart';
 import 'package:redux/redux.dart';
-
+import 'package:redux_thunk/redux_thunk.dart';
 import 'Model/Snuff.dart';
 import 'Redux/app_state.dart';
+import 'Config/app_routes.dart';
+import 'Redux/reducer.dart';
+import 'Styles/app_colors.dart';
 
 void main() {
   runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  final Store<AppState> _store = Store<AppState>(updatedSnuffReducer,
-      initialState: AppState(
-        snuffs: [
-          Snuff("1", "Skruf", "Original Portion2", 50, 20,
-              "assets/images/skruf.png", 1, false),
-          Snuff("2", "Skruf", "Cranberry Portion", 50, 20,
-              "assets/images/skruf.png", 1, false),
-          Snuff("3", "Skruf", "Fresh Portion", 50, 20,
-              "assets/images/skruf.png", 1, false),
-          Snuff("4", "Skruf", "Slim Fresh White Portion", 50, 20,
-              "assets/images/skruf.png", 1, false),
-          Snuff("5", "General", "Original Portion", 50, 20,
-              "assets/images/general.png", 1, false),
-          Snuff("6", "General", "Mint Portion", 50, 20,
-              "assets/images/general.png", 1, false),
-          Snuff("7", "General", "Wintergreen Portion", 50, 20,
-              "assets/images/general.png", 1, false),
-          Snuff("8", "General", "White Portion", 50, 20,
-              "assets/images/general.png", 1, false),
-        ],
-      ));
+  final Store<AppState> _store = Store<AppState>(
+    appReducer,
+    initialState: AppState(
+      snuffs: [
+        Snuff("1", "Skruf", "Original Portion2", 50, 20,
+            "assets/images/skruf.png", 1, false),
+        Snuff("7", "General", "Wintergreen Portion", 50, 20,
+            "assets/images/general.png", 1, false),
+        Snuff("8", "General", "White Portion", 50, 20,
+            "assets/images/general.png", 1, false),
+      ],
+    ),
+    middleware: [thunkMiddleware], // Lägg till thunkMiddleware här
+  );
+
   MainApp({super.key});
 
   @override
