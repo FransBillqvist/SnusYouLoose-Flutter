ee6033efde5fbbf1c5f6f7e9b8f1b17707b5d596
Frans Billqvist <frans.billquist@gmail.com>
2023-12-17 20:35:44 +0100

    Add selectedSnuff property to AppState class

--- snusyoulooseflutter/lib/Redux/app_state.dart
+++ snusyoulooseflutter/lib/Redux/app_state.dart
@@ -4,8 +4,9 @@ import '../Model/User.dart';
 
 class AppState {
   List<Snuff> snuffs;
+  Snuff? selectedSnuff;
   Habit? habit;
   User? user;
 
-  AppState({this.snuffs = const [], this.habit, this.user});
+  AppState({this.snuffs = const [], this.selectedSnuff, this.habit, this.user});
 }
