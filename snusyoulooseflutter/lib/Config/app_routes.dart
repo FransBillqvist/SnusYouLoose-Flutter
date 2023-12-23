59db9c801d17d661acdf2fe556e5822baf2cb299
Frans Billqvist <frans.billquist@gmail.com>
2023-12-23 21:47:22 +0100

    Refactor app_routes.dart to include authentication check

--- snusyoulooseflutter/lib/Config/app_routes.dart
+++ snusyoulooseflutter/lib/Config/app_routes.dart
@@ -1,12 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+
 import '../Pages/home_page.dart';
 import '../Pages/login_page.dart';
+import '../Redux/app_state.dart';
 
 class AppRoutes {
   static final pages = {
     login: (context) => LoginPage(),
-    home: (context) => HomePage(),
+    home: (context) => _checkAuth(context, HomePage()),
   };
 
   static const String login = '/';
   static const String home = '/home';
+
+  static Widget _checkAuth(BuildContext context, Widget page) {
+    var store = StoreProvider.of<AppState>(context);
+    return store.state.user != null ? page : LoginPage();
+  }
 }
