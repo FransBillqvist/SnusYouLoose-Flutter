47896c8e5d4bcce80b39ca77793f74bc5116cc0d
Frans Billqvist <frans.billquist@gmail.com>
2023-12-26 21:07:08 +0100

    Refactor AppState class to use inventorySnuffs instead of snuffs

--- snusyoulooseflutter/lib/Redux/app_state.dart
+++ snusyoulooseflutter/lib/Redux/app_state.dart
@@ -5,10 +5,14 @@ import '../Model/Snuff.dart';
 import '../Model/User.dart';
 
 class AppState {
-  List<CurrentSnuff> snuffs;
+  List<CurrentSnuff> inventorySnuffs;
   Snuff? selectedSnuff;
   Habit? habit;
   User? user;
 
-  AppState({this.snuffs = const [], this.selectedSnuff, this.habit, this.user});
+  AppState(
+      {this.inventorySnuffs = const [],
+      this.selectedSnuff,
+      this.habit,
+      this.user});
 }
