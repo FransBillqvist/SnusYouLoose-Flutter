2ad0da935467ba338792c097117eac9287edfc95
Frans Billqvist <frans.billquist@gmail.com>
2023-12-26 21:06:50 +0100

    Add fetchSnuff API endpoint

--- snusyoulooseflutter/lib/Config/app_urls.dart
+++ snusyoulooseflutter/lib/Config/app_urls.dart
@@ -5,4 +5,5 @@ class AppUrls {
   static const fetchUserGateway = '$_url/api/v2/User/Get/';
   static const fetchHabitGateway = '$_url/api/v2/Habit/Get/';
   static const fetchUsersInventory = '$_url/api/v1/CurrentSnuff/GetAllSnuff/';
+  static const fetchSnuff = '$_url/api/v2/Snuff/Get/';
 }
