5851c38d04185d0f1d4267282e07ade106f3fef7
Frans Billqvist <frans.billquist@gmail.com>
2024-01-18 04:17:23 +0100

    Update habit_page.dart with new UI layout

--- snusyoulooseflutter/lib/Pages/habit_page.dart
+++ snusyoulooseflutter/lib/Pages/habit_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:snusyoulooseflutter/Components/app_iconbutton.dart';
+import 'package:snusyoulooseflutter/Config/app_media.dart';
+import 'package:snusyoulooseflutter/Config/app_strings.dart';
 import 'package:snusyoulooseflutter/Styles/app_colors.dart';
 
 class HabitPage extends StatelessWidget {
@@ -7,10 +10,75 @@ class HabitPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Habit Page",
-          style: TextStyle(color: AppColors.example1),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column(children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 32),
+                  child: SizedBox(
+                    width: 250,
+                    height: 226,
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          AppImages.logoWithoutBg,
+                          fit: BoxFit.contain,
+                          height: 120,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: Text(
+                            AppStrings.habitInfo,
+                            style: TextStyle(
+                                fontSize: 15, color: AppColors.textOnFocus),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: SizedBox(
+                width: 400,
+                height: 400,
+                child: Container(
+                    decoration: BoxDecoration(
+                      color: AppColors.example2,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              print('YOU PRESSED: APP');
+                            },
+                            child: Text(
+                              AppStrings.appMode,
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: AppColors.textOnFocus,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )),
+              ),
+            ),
+          ]),
         ),
       ),
     );
