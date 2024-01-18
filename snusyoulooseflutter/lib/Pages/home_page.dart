0a5ffb18f300b7b914e92c7384b00e49fd20a27b
Frans Billqvist <frans.billquist@gmail.com>
2024-01-18 03:12:24 +0100

    Refactor home_page.dart

--- snusyoulooseflutter/lib/Pages/home_page.dart
+++ snusyoulooseflutter/lib/Pages/home_page.dart
@@ -24,6 +24,10 @@ class _HomePageState extends State<HomePage> {
     final store = StoreProvider.of<AppState>(context);
     final userid = store.state.user?.UserId.toString();
     store.dispatch(getSnuffInventory(userid!));
+    store.dispatch(getUserHabit(userid, context));
+    // if (store.state.habit?.doseType == "ERROR") {
+    //   Navigator.of(context).pushReplacementNamed(AppRoutes.habit);
+    // }
     return Scaffold(
       body: SingleChildScrollView(
         child: StoreConnector<AppState, List<CurrentSnuffDto>>(
@@ -46,25 +50,10 @@ class _HomePageState extends State<HomePage> {
                     height: MediaQuery.of(context).size.height * 0.4,
                     child: InventoryWidget(currentSnuffs: stateSnuff)),
               ),
-              // ...stateSnuff
-              //     .map(
-              //       (snuff) => SnuffWidget(
-              //         snuffs: snuff,
-              //         onChanged: (onChange) {
-              //           snuff.selected = !snuff.selected;
-              //           StoreProvider.of<AppState>(context)
-              //               .dispatch(UpdateSnuffAction(snuff));
-              //         },
-              //         key: ValueKey(snuff.id),
-              //       ),
-              //     )
-              //     .toList(),
               ElevatedButton(
                 onPressed: () async {
                   var store = StoreProvider.of<AppState>(context);
                   var current = store.state;
-                  // print(current.user);
-                  // print("Line Is 124");
                   if (current.user != null) {
                     store.dispatch(SignOutUserAction(current.user!));
                     Navigator.of(context).pushReplacementNamed(AppRoutes.login);
@@ -87,41 +76,4 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-
-  // Future<User> fetchUserInfo(String userId) async {
-  //   // Make API call with the authUserId
-  //   final response = await http.post(Uri.parse(AppUrls.fetchUserGateway),
-  //       body: {'authUserId': userId});
-
-  //   // Handle the response
-  //   if (response.statusCode == 200) {
-  //     // Successful login
-  //     final responseData = jsonDecode(response.body);
-  //     final user = User.fromJson(responseData);
-  //     print('Successfully fetched user info');
-  //     return user;
-  //   } else {
-  //     print('Failed to fetch user info');
-  //     throw Exception('Failed to fetch user info');
-  //   }
-  // }
-
-  // Future<Habit> fetchUserHabit(String userId) async {
-  //   // Make API call with the authUserId
-  //   final response = await http.post(Uri.parse(AppUrls.fetchHabitGateway),
-  //       body: {'authUserId': userId});
-
-  //   // Handle the response
-  //   if (response.statusCode == 200) {
-  //     final responseData = jsonDecode(response.body);
-  //     final habit = Habit.fromJson(responseData);
-  //     print('SUCCESFULLY fetched user habit');
-  //     return habit;
-  //   } else {
-  //     // Failed login
-  //     print('FAILED to fetch user habit');
-  //     throw Exception('Failed to fetch user habit');
-  //     // return Habit('','',-999,'','',-999,DateTime.parse('1970-01-12'),DateTime.parse('1970-01-12'),'','');
-  //   }
-  // }
 }
