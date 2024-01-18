715a290cb3330a53ceedefa4a6d6476bfe47a76b
Frans Billqvist <frans.billquist@gmail.com>
2024-01-18 03:11:58 +0100

    Add habit page to app routes

--- snusyoulooseflutter/lib/Config/app_routes.dart
+++ snusyoulooseflutter/lib/Config/app_routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 
+import '../Pages/habit_page.dart';
 import '../Pages/home_page.dart';
 import '../Pages/login_page.dart';
 import '../Redux/app_state.dart';
@@ -9,10 +10,12 @@ class AppRoutes {
   static final pages = {
     login: (context) => LoginPage(),
     home: (context) => _checkAuth(context, HomePage()),
+    habit: (context) => _checkAuth(context, HabitPage())
   };
 
   static const String login = '/';
   static const String home = '/home';
+  static const String habit = '/habit';
 
   static Widget _checkAuth(BuildContext context, Widget page) {
     var store = StoreProvider.of<AppState>(context);
