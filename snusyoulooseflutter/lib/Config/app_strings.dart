5fd166b7ba18cca05069629230609c6c3846ad28
Frans Billqvist <frans.billquist@gmail.com>
2024-01-22 02:17:24 +0100

    Add new strings to AppStrings class

--- snusyoulooseflutter/lib/Config/app_strings.dart
+++ snusyoulooseflutter/lib/Config/app_strings.dart
@@ -12,10 +12,13 @@ class AppStrings {
   static const String noInventory = 'You got no snuff in your inventory';
   static const String snuffsLeft = 'snuffs left';
   static const String snuffLeft = 'snuff left';
+  static const String snuffBox = 'snuffbox';
+  static const String portions = 'portions of snuff';
   static const String thereis = 'There is';
   static const String habitInfo = 'Now you need to choose wich mode to use.';
   static const String appMode = 'App Mode';
   static const String quest0 = 'Select what time of the day you usally wake up';
   static const String quest1 =
       'Select what time of the day you usally go to sleep';
+  static const String quest2 = 'How many snuffs do you use per day?';
 }
