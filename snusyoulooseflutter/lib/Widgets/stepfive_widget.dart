3a1931cbb4a2f253880a11c20449fe00ed323965
Frans Billqvist <frans.billquist@gmail.com>
2024-01-23 07:49:53 +0100

    Refactor StepFiveWidget to include dynamic content

--- snusyoulooseflutter/lib/Widgets/stepfive_widget.dart
+++ snusyoulooseflutter/lib/Widgets/stepfive_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../Config/app_strings.dart';
+
 class StepFiveWidget extends StatefulWidget {
   final ValueChanged<dynamic> onFinalChanged;
   final int stepIndecator;
@@ -13,6 +15,30 @@ class StepFiveWidget extends StatefulWidget {
 class _StepFiveWidgetState extends State<StepFiveWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: 300,
+        child: Padding(
+          padding: EdgeInsets.only(right: 16.0, left: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              if (widget.stepIndecator == 4)
+                Container(
+                  child: Text(AppStrings.infoAppMode),
+                ),
+              if (widget.stepIndecator == 5)
+                Container(
+                  child: Text(
+                    AppStrings.infoDateMode,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
