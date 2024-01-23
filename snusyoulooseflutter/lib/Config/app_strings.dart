d6d22ff823c70efc76e13b6e2e03a216c217103b
Frans Billqvist <frans.billquist@gmail.com>
2024-01-23 01:06:10 +0100

    Add new strings for date and reduce modes

--- snusyoulooseflutter/lib/Config/app_strings.dart
+++ snusyoulooseflutter/lib/Config/app_strings.dart
@@ -19,8 +19,11 @@ class AppStrings {
   static const String thereis = 'There is';
   static const String habitInfo = 'Now you need to choose wich mode to use.';
   static const String appMode = 'App Mode';
+  static const String dateMode = 'Date Mode';
+  static const String reduceMode = 'Reduce Mode'; //TODO
   static const String quest0 = 'Select what time of the day you usally wake up';
   static const String quest1 =
       'Select what time of the day you usally go to sleep';
   static const String quest2 = 'How many snuffs do you use per day?';
+  static const String quest3 = 'Choose what type of habit change you want';
 }
