98177b886288f73ff4ab74ee9cd793ca83781d73
Frans Billqvist <frans.billquist@gmail.com>
2024-01-23 23:23:21 +0100

    Add wakeUpTime, sleepTime, modeType, amountOfSnuff, and doseType variables to StepSixWidget Base V

--- snusyoulooseflutter/lib/Widgets/stepsix_widget.dart
+++ snusyoulooseflutter/lib/Widgets/stepsix_widget.dart
@@ -1,15 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:snusyoulooseflutter/Widgets/stepfour_widget.dart';
 
 import '../Config/app_strings.dart';
 
 class StepSixWidget extends StatefulWidget {
-  const StepSixWidget({super.key});
+  late DateTime wakeUpTime = DateTime(0);
+  late DateTime sleepTime = DateTime(0);
+  late AppMode modeType;
+  late int? speed;
+  late int amountOfSnuff;
+  late String doseType;
+  final DateTime startDate = DateTime.now();
+  final DateTime? EndDate;
+  StepSixWidget(
+      {required this.wakeUpTime,
+      required this.sleepTime,
+      required this.modeType,
+      required this.amountOfSnuff,
+      required this.doseType,
+      this.speed,
+      this.EndDate});
 
   @override
   State<StepSixWidget> createState() => _StepSixWidgetState();
 }
 
+String getDose(String doseType) {
+  switch (doseType) {
+    case 'prillor':
+      return 'Portions';
+    case 'dosor':
+      return 'Boxes';
+    case _:
+      return 'Snuff';
+  }
+}
+
+String getMode(AppMode modeType) {
+  switch (modeType) {
+    case AppMode.appMode:
+      return 'App';
+    case AppMode.dateMode:
+      return 'Date';
+    case AppMode.NONE:
+      return 'NONE';
+  }
+}
+
 class _StepSixWidgetState extends State<StepSixWidget> {
+  final double textSize = 18;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -21,12 +61,69 @@ class _StepSixWidgetState extends State<StepSixWidget> {
             AppStrings.recapHabit,
             style: TextStyle(fontSize: 20),
           ),
+          Padding(
+            padding: EdgeInsets.only(top: 40),
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
-                AppStrings.quest2,
-                style: TextStyle(fontSize: 18),
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(6.0),
+                    child: Text(AppStrings.wakeUpTime,
+                        style: TextStyle(fontSize: 16)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(6.0),
+                    child: Text(AppStrings.sleepTime,
+                        style: TextStyle(fontSize: 16)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(6.0),
+                    child: Text(AppStrings.doseType,
+                        style: TextStyle(fontSize: 16)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(6.0),
+                    child: Text(AppStrings.amountOf,
+                        style: TextStyle(fontSize: 16)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(6.0),
+                    child: Text(AppStrings.modeSelected,
+                        style: TextStyle(fontSize: 16)),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20),
+              ),
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 4.0),
+                    child: Text(DateFormat('HH:mm').format(widget.wakeUpTime),
+                        style: TextStyle(fontSize: 18)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(6.0),
+                    child: Text(DateFormat('HH:mm').format(widget.sleepTime),
+                        style: TextStyle(fontSize: 18)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(6.0),
+                    child: Text(getDose(widget.doseType),
+                        style: TextStyle(fontSize: 18)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(6.0),
+                    child: Text(widget.amountOfSnuff.toString(),
+                        style: TextStyle(fontSize: 18)),
+                  ),
+                  Text(getMode(widget.modeType),
+                      style: TextStyle(fontSize: 18)),
+                ],
               ),
             ],
           ),
