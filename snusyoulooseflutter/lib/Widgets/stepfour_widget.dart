e03dabe700b470872eaa868b202c7b6cabde7cbd
Frans Billqvist <frans.billquist@gmail.com>
2024-01-23 01:08:31 +0100

    Add StepFourWidget Base

---
+++ snusyoulooseflutter/lib/Widgets/stepfour_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StepFourWidget extends StatefulWidget {
+  const StepFourWidget({super.key});
+
+  @override
+  State<StepFourWidget> createState() => _StepFourWidgetState();
+}
+
+class _StepFourWidgetState extends State<StepFourWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
