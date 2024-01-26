b12849a24bad2968f66fda767836c088c82e6139
Frans Billqvist <frans.billquist@gmail.com>
2024-01-26 05:02:53 +0100

    Add ProgressionDto class for handling progression data

---
+++ snusyoulooseflutter/lib/Model/ProgressionDto.dart
@@ -0,0 +1,33 @@
+class ProgressionDto {
+  final DateTime goalStartDate;
+  final DateTime goalEndDate;
+  final int snuffLimitAmount;
+  final Duration recommendedUsageInterval;
+  final bool inUse;
+
+  ProgressionDto(
+      {required this.goalStartDate,
+      required this.goalEndDate,
+      required this.snuffLimitAmount,
+      required this.recommendedUsageInterval,
+      required this.inUse});
+
+  factory ProgressionDto.fromJson(Map<String, dynamic> json) => ProgressionDto(
+        goalStartDate: DateTime.parse(json['goalStartDate']),
+        goalEndDate: DateTime.parse(json['goalEndDate']),
+        snuffLimitAmount: json['snuffLimitAmount'],
+        recommendedUsageInterval:
+            Duration(seconds: json['recommendedUsageInterval']),
+        inUse: json['inUse'],
+      );
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['goalStartDate'] = goalStartDate.toIso8601String();
+    data['goalEndDate'] = goalEndDate.toIso8601String();
+    data['snuffLimitAmount'] = snuffLimitAmount;
+    data['recommendedUsageInterval'] = recommendedUsageInterval;
+    data['inUse'] = inUse;
+    return data;
+  }
+}
