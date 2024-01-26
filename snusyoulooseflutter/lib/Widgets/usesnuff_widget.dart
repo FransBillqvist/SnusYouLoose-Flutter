b87b22c4a70aff5828434b9f20a0e9b899a161de
Frans Billqvist <frans.billquist@gmail.com>
2024-01-26 06:41:10 +0100

    Add snuff selection dialog

--- snusyoulooseflutter/lib/Widgets/usesnuff_widget.dart
+++ snusyoulooseflutter/lib/Widgets/usesnuff_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:snusyoulooseflutter/Styles/app_colors.dart';
+
+import '../Config/app_strings.dart';
 
 class UseSnuffWidget extends StatelessWidget {
   late String currentsnuffID;
@@ -7,26 +10,32 @@ class UseSnuffWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Future.delayed(Duration.zero, () => _showDialog(context));
-    return const Placeholder();
+    return Container(child: Text("test"));
   }
 
-  _showDialog(BuildContext context) {
+  void _showDialog(BuildContext context) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         int selectedNumber = 1;
         return AlertDialog(
-          title: Text('Select a number'),
+          title: const Text(AppStrings.takenSnuffAmount),
+          backgroundColor: AppColors.example2,
+          contentPadding: const EdgeInsets.all(18.0),
           content: DropdownButton<int>(
             value: selectedNumber,
             onChanged: (int? newValue) {
               selectedNumber = newValue!;
-              Navigator.of(context).pop();
+              Navigator.of(context);
             },
             items: <int>[1, 2, 3, 4, 5].map<DropdownMenuItem<int>>((int value) {
               return DropdownMenuItem<int>(
                 value: value,
-                child: Text(value.toString()),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 18.0),
+                  child: Text(value.toString()),
+                ),
+                onTap: () {},
               );
             }).toList(),
           ),
