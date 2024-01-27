9f9bae57715a02170843dd5a52f9d7fa8f4455c4
Frans Billqvist <frans.billquist@gmail.com>
2024-01-27 01:19:54 +0100

    Refactor UseSnuffWidget to use a StatefulWidget and extract NumberDropdown as a separate widget

--- snusyoulooseflutter/lib/Widgets/usesnuff_widget.dart
+++ snusyoulooseflutter/lib/Widgets/usesnuff_widget.dart
@@ -1,46 +1,188 @@
 import 'package:flutter/material.dart';
+import 'package:snusyoulooseflutter/Pages/home_page.dart';
 import 'package:snusyoulooseflutter/Styles/app_colors.dart';
 
 import '../Config/app_strings.dart';
 
-class UseSnuffWidget extends StatelessWidget {
-  late String currentsnuffID;
+class UseSnuffWidget extends StatefulWidget {
+  final String currentsnuffID;
+
   UseSnuffWidget({required this.currentsnuffID});
 
   @override
+  _UseSnuffWidgetState createState() => _UseSnuffWidgetState();
+}
+
+class _UseSnuffWidgetState extends State<UseSnuffWidget> {
+  int selectedNumber = 1;
+  bool isDialogShowing = false;
+
+  @override
   Widget build(BuildContext context) {
-    Future.delayed(Duration.zero, () => _showDialog(context));
-    return Container(child: Text("test"));
+    if (!isDialogShowing) {
+      Future.delayed(Duration(milliseconds: 300), () => _showDialog(context));
+    }
+    return HomePage();
   }
 
   void _showDialog(BuildContext context) {
+    isDialogShowing = true;
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        int selectedNumber = 1;
         return AlertDialog(
           title: const Text(AppStrings.takenSnuffAmount),
           backgroundColor: AppColors.example2,
           contentPadding: const EdgeInsets.all(18.0),
-          content: DropdownButton<int>(
-            value: selectedNumber,
-            onChanged: (int? newValue) {
-              selectedNumber = newValue!;
-              Navigator.of(context);
-            },
-            items: <int>[1, 2, 3, 4, 5].map<DropdownMenuItem<int>>((int value) {
-              return DropdownMenuItem<int>(
-                value: value,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 18.0),
-                  child: Text(value.toString()),
+          content: NumberDropdown(),
+          actions: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 118.0),
+              child: TextButton(
+                child: const Text(
+                  AppStrings.close,
+                  style: TextStyle(color: Colors.red),
                 ),
-                onTap: () {},
-              );
-            }).toList(),
-          ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ),
+            TextButton(
+              child: const Text(
+                AppStrings.ok,
+                style: TextStyle(fontSize: 18, color: AppColors.primary),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
         );
       },
+    ).then((_) => isDialogShowing = false);
+  }
+}
+
+class NumberDropdown extends StatefulWidget {
+  @override
+  _NumberDropdownState createState() => _NumberDropdownState();
+}
+
+class _NumberDropdownState extends State<NumberDropdown> {
+  int selectedNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<int>(
+      value: selectedNumber,
+      onChanged: (int? newValue) {
+        setState(() {
+          selectedNumber = newValue!;
+        });
+      },
+      items: <int>[1, 2, 3, 4, 5].map<DropdownMenuItem<int>>((int value) {
+        return DropdownMenuItem<int>(
+          value: value,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 18.0),
+            child: Text(value.toString()),
+          ),
+        );
+      }).toList(),
     );
   }
 }
+
+
+
+// class UseSnuffWidget extends StatefulWidget {
+//   final String currentsnuffID;
+
+//   UseSnuffWidget({required this.currentsnuffID});
+
+//   @override
+//   _UseSnuffWidgetState createState() => _UseSnuffWidgetState();
+// }
+
+// class _UseSnuffWidgetState extends State<UseSnuffWidget> {
+//   int selectedNumber = 1;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     Future.delayed(Duration.zero, () => _showDialog(context));
+//     return HomePage();
+//   }
+
+//   void _showDialog(BuildContext context) {
+//     showDialog(
+//       context: context,
+//       builder: (BuildContext context) {
+//         return AlertDialog(
+//           title: const Text(AppStrings.takenSnuffAmount),
+//           backgroundColor: AppColors.example2,
+//           contentPadding: const EdgeInsets.all(18.0),
+//           content: DropdownButton<int>(
+//             value: selectedNumber,
+//             onChanged: (int? newValue) {
+//               setState(() {
+//                 selectedNumber = newValue!;
+//               });
+//             },
+//             items: <int>[1, 2, 3, 4, 5].map<DropdownMenuItem<int>>((int value) {
+//               return DropdownMenuItem<int>(
+//                 value: value,
+//                 child: Padding(
+//                   padding: const EdgeInsets.only(left: 18.0),
+//                   child: Text(value.toString()),
+//                 ),
+//               );
+//             }).toList(),
+//           ),
+//         );
+//       },
+//     );
+//   }
+// }
+
+
+
+// class UseSnuffWidget extends StatelessWidget {
+//   late String currentsnuffID;
+//   UseSnuffWidget({required this.currentsnuffID});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     Future.delayed(Duration.zero, () => _showDialog(context));
+//     return HomePage();
+//   }
+
+//   void _showDialog(BuildContext context) {
+//     showDialog(
+//       context: context,
+//       builder: (BuildContext context) {
+//         int selectedNumber = 1;
+//         return AlertDialog(
+//           title: const Text(AppStrings.takenSnuffAmount),
+//           backgroundColor: AppColors.example2,
+//           contentPadding: const EdgeInsets.all(18.0),
+//           content: DropdownButton<int>(
+//             value: selectedNumber,
+//             onChanged: (int? newValue) {
+//               selectedNumber = newValue!;
+//             },
+//             items: <int>[1, 2, 3, 4, 5].map<DropdownMenuItem<int>>((int value) {
+//               return DropdownMenuItem<int>(
+//                 value: value,
+//                 child: Padding(
+//                   padding: const EdgeInsets.only(left: 18.0),
+//                   child: Text(value.toString()),
+//                 ),
+//               );
+//             }).toList(),
+//           ),
+//         );
+//       },
+//     );
+//   }
+// }
