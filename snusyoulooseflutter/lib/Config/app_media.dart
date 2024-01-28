d4055864e173319ae18a79945d191b43b5b0036b
Frans Billqvist <frans.billquist@gmail.com>
2024-01-28 05:43:24 +0100

    Add new snuff images

--- snusyoulooseflutter/lib/Config/app_media.dart
+++ snusyoulooseflutter/lib/Config/app_media.dart
@@ -22,3 +22,76 @@ class AppIcons {
   static const timer_v2 = '${_icn}timer_v2.svg';
   static const timer_v2_ = '${_icn}timer_v2_.svg';
 }
+
+class AppSnuffs {
+  static const _snf = 'Assets/Images/snuff/';
+
+  static const acexraspberrychilistrong =
+      '${_snf}ace-x-raspberry-chili-strong.webp';
+  static const apresiceteapeach = '${_snf}apres-ice-tea-peach.webp';
+  static const fixpineapplerumcoconut4 =
+      '${_snf}fix-pineapple-rum-coconut-4.webp';
+  static const helwitcherryslim = '${_snf}helwit-cherry-slim.webp';
+  static const helwitmintslim = '${_snf}helwit-mint-slim.webp';
+  static const helwitmochaslim = '${_snf}helwit-mocha-slim.webp';
+  static const helwitorangeslim = '${_snf}helwit-orange-slim.webp';
+  static const helwitslimblueberry = '${_snf}helwit-slim-blueberry.webp';
+  static const helwitslimviolet = '${_snf}helwit-slim-violet.webp';
+  static const kellywhiteraspberrylemonmini =
+      '${_snf}kelly-white-raspberry-lemon-mini.webp';
+  static const kellywhitesparklingstrawberryslim =
+      '${_snf}kelly-white-sparkling-strawberry-slim.webp';
+  static const lundgrensvagsvallallwhite =
+      '${_snf}lundgrens-vagsvall-all-white.webp';
+  static const stromicedcoffeeslim = '${_snf}strom-iced-coffee-slim.webp';
+  static const velo14mgbreezymango = '${_snf}velo-14mg-breezy-mango.webp';
+  static const veloorangespark = '${_snf}velo-orange-spark.webp';
+  static const velotangledberry = '${_snf}velo-tangled-berry.webp';
+  static const velotropicbreezezeronfsnuff =
+      '${_snf}velo-tropic-breeze-zero-nf-snuff.webp';
+  static const velozestflame = '${_snf}velo-zest-flame.webp';
+  static const xqsblackcherryslimstrong =
+      '${_snf}xqs-black-cherry-slim-strong.webp';
+  static const xqsblueberrymintlightallwhiteportion =
+      '${_snf}xqs-blueberry-mint-light-all-white-portion.webp';
+  static const xqscactussourlightallwhiteportion =
+      '${_snf}xqs-cactus-sour-light-all-white-portion.webp';
+  static const xqscactussourslimstrong =
+      '${_snf}xqs-cactus-sour-slim-strong.webp';
+  static const xqscitruscoolingslim = '${_snf}xqs-citrus-cooling-slim.webp';
+  static const xqselderflowerslimstrong =
+      '${_snf}xqs-elderflower-slim-strong.webp';
+  static const xqsfizzycolaallwhiteportion =
+      '${_snf}xqs-fizzy-cola-all-white-portion.webp';
+  static const xqsfizzycolastrong = '${_snf}xqs-fizzy-cola-strong.webp';
+  static const xqsorangeappleslimstrong =
+      '${_snf}xqs-orange-apple-slim-strong.webp';
+  static const xqsslimstrongblueberrymint =
+      '${_snf}xqs-slim-strong-blueberry-mint.webp';
+  static const xqsslimstrongwintergreen =
+      '${_snf}xqs-slim-strong-wintergreen.webp';
+  static const xqssofttoffeeslimstrong =
+      '${_snf}xqs-soft-toffee-slim-strong.webp';
+  static const xqstropicalslimextrastrongallwhiteportion =
+      '${_snf}xqs-tropical-slim-extra-strong-all-white-portion.webp';
+  static const xqstropicalslimnormalallwhiteportion =
+      '${_snf}xqs-tropical-slim-normal-all-white-portion.webp';
+  static const xqstwinappleslimstrong =
+      '${_snf}xqs-twin-apple-slim-strong.webp';
+  static const xqswintergreenslim = '${_snf}xqs-wintergreen-slim.webp';
+  static const xrgeneralslimwhiteportionssnus =
+      '${_snf}xr-general-slim-white-portionssnus.webp';
+  static const zonexcoldblastslimextrastrongallwhiteport =
+      '${_snf}zone-x-cold-blast-slim-extra-strong-all-white-port.webp';
+  static const zonexcosmicblastslimextrastrongallwhitepor =
+      '${_snf}zonex-cosmic-blast-slim-extra-strong-all-white-por.webp';
+  static const zynminiblackcherry3mg = '${_snf}zyn-mini-black-cherry-3-mg.webp';
+  static const zynminicitrus3mg = '${_snf}zyn-mini-citrus-3-mg.webp';
+  static const zynminicoolmint3mg = '${_snf}zyn-mini-cool-mint-3-mg.webp';
+  static const zynminiespressino = '${_snf}zyn-mini-espressino.webp';
+  static const zynslimspearmintstrong = '${_snf}zyn-slim-spearmint-strong.webp';
+  static const zynslimstrongapplemint =
+      '${_snf}zyn-slim-strong-apple-mint.webp';
+  static const zynspearmintminidry = '${_snf}zyn-spearmint-mini-dry.webp';
+  static const zynvioletlicoriceslim = '${_snf}zyn-violet-licorice-slim.webp';
+}
