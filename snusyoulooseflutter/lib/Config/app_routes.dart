f4282b73fbc5fd01e66d0f1769acfed30c5885e7
Frans Billqvist <frans.billquist@gmail.com>
2024-01-28 22:39:54 +0100

    Add shop page to app_routes.dart

--- snusyoulooseflutter/lib/Config/app_routes.dart
+++ snusyoulooseflutter/lib/Config/app_routes.dart
@@ -4,18 +4,21 @@ import 'package:flutter_redux/flutter_redux.dart';
 import '../Pages/habit_page.dart';
 import '../Pages/home_page.dart';
 import '../Pages/login_page.dart';
+import '../Pages/shop_page.dart';
 import '../Redux/app_state.dart';
 
 class AppRoutes {
   static final pages = {
     login: (context) => LoginPage(),
     home: (context) => _checkAuth(context, HomePage()),
-    habit: (context) => _checkAuth(context, HabitPage())
+    habit: (context) => _checkAuth(context, HabitPage()),
+    shop: (context) => _checkAuth(context, ShopPage()),
   };
 
   static const String login = '/';
   static const String home = '/home';
   static const String habit = '/habit';
+  static const String shop = '/shop';
 
   static Widget _checkAuth(BuildContext context, Widget page) {
     var store = StoreProvider.of<AppState>(context);
