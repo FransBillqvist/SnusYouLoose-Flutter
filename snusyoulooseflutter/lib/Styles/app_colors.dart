b189d3637c56d3706b93f8a9cf41e0d747d48be2
Frans Billqvist <frans.billquist@gmail.com>
2024-01-29 01:17:24 +0100

    Add new colors to AppColors

--- snusyoulooseflutter/lib/Styles/app_colors.dart
+++ snusyoulooseflutter/lib/Styles/app_colors.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const primary = Color(0xFF0D47A1);
   static const example1 = Color(0xFF5D737E);
+  static const exampleback1 = Color(0xFF5D737E);
   static const example2 = Color(0xFF64B6AC);
   static const example3 = Color(0xFFC0FDFB);
   static const example4 = Color(0xFFDAFFEF);
   static const example5 = Color(0xFFFCFFFD);
   static final textFieldHintColor = const Color(0xffD9D9D9).withOpacity(0.6);
   static const textOnFocus = Color.fromARGB(178, 44, 15, 232);
+  static const textOnFocus2 = Color.fromARGB(211, 223, 238, 10);
   static const loginDeepBackgroundColor = Color(0xFF133754);
   static const loginLightBackgroundColor = Color(0xFF2E5B69);
   static const loginDeepTextColor = Color(0xFFC2E2D6);
