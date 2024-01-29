e9f30dd21326a275fa6d32bb28549e528306522a
Frans Billqvist <frans.billquist@gmail.com>
2024-01-29 02:53:46 +0100

    Update exampleback1 color in AppColors

--- snusyoulooseflutter/lib/Styles/app_colors.dart
+++ snusyoulooseflutter/lib/Styles/app_colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const primary = Color(0xFF0D47A1);
   static const example1 = Color(0xFF5D737E);
-  static const exampleback1 = Color(0xFF5D737E);
+  static const exampleblack1 = Color(0x272727);
   static const example2 = Color(0xFF64B6AC);
   static const example3 = Color(0xFFC0FDFB);
   static const example4 = Color(0xFFDAFFEF);
