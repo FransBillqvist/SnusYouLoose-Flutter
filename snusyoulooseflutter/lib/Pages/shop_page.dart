875579e053c7fedc764b91249f6013b7bd1e24a1
Frans Billqvist <frans.billquist@gmail.com>
2024-01-30 02:00:51 +0100

    Refactor shop page layout and add shopping cart button with indicator, backbutton to HomePage

--- snusyoulooseflutter/lib/Pages/shop_page.dart
+++ snusyoulooseflutter/lib/Pages/shop_page.dart
@@ -6,9 +6,17 @@ import 'package:snusyoulooseflutter/Services/app_services.dart';
 import '../Styles/app_colors.dart';
 import '../Widgets/shop_widget.dart';
 
-class ShopPage extends StatelessWidget {
+class ShopPage extends StatefulWidget {
   ShopPage({super.key});
+
+  @override
+  State<ShopPage> createState() => _ShopPageState();
+  int numberOfItemsInCart = 0;
+}
+
+class _ShopPageState extends State<ShopPage> {
   final shop = fetchSnuffShopService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,12 +28,28 @@ class ShopPage extends StatelessWidget {
             children: [
               Row(
                 children: [
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                        Colors.transparent,
+                      ),
+                      elevation: MaterialStateProperty.all(0),
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Icon(
+                      Icons.arrow_back,
+                      size: 28,
+                      color: Colors.indigoAccent[200],
+                    ),
+                  ),
                   Center(
                     child: Padding(
-                      padding: const EdgeInsets.only(top: 8.0, left: 18.0),
+                      padding: const EdgeInsets.only(top: 24.0, left: 17),
                       child: SizedBox(
-                        height: 104,
-                        width: MediaQuery.of(context).size.width * 0.91,
+                        height: 64,
+                        width: MediaQuery.of(context).size.width * 0.61,
                         child: Material(
                           elevation:
                               14.0, // Change this value to adjust the elevation
@@ -35,14 +59,56 @@ class ShopPage extends StatelessWidget {
                             child: Text(
                               "Add a Search-Function HERE",
                               style: TextStyle(
-                                  color: AppColors.textOnFocus2, fontSize: 24),
+                                  color: AppColors.textOnFocus2, fontSize: 16),
                             ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ],
+                  Padding(
+                      padding: const EdgeInsets.only(top: 5.0),
+                      child: Stack(
+                        children: <Widget>[
+                          IconButton(
+                            icon: Icon(Icons.shopping_cart),
+                            color: (widget.numberOfItemsInCart <= 0
+                                ? Colors.white
+                                : Colors.indigoAccent[200]),
+                            onPressed: () {
+                              // Handle the press event
+                            },
+                          ),
+                          if (widget.numberOfItemsInCart > 0)
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 8.0, left: 26),
+                              child: Positioned(
+                                right: 0,
+                                child: Container(
+                                  padding: EdgeInsets.all(1),
+                                  decoration: BoxDecoration(
+                                    color: Colors.red,
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                  constraints: BoxConstraints(
+                                    minWidth: 12,
+                                    minHeight: 12,
+                                  ),
+                                  child: Text(
+                                    '${widget.numberOfItemsInCart}',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 12,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ),
+                            ),
+                        ],
+                      )),
+                ], //Row ends HERE:
               ),
               SizedBox(
                 height: MediaQuery.of(context).size.height,
