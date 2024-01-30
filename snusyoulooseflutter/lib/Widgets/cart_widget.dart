d52afbc774089305994d3e733b7f5a3c1f2e571d
Frans Billqvist <frans.billquist@gmail.com>
2024-01-30 07:55:16 +0100

    Add exit button to CartWidget

--- snusyoulooseflutter/lib/Widgets/cart_widget.dart
+++ snusyoulooseflutter/lib/Widgets/cart_widget.dart
@@ -4,7 +4,8 @@ import '../Model/CreateCSDto.dart';
 
 class CartWidget extends StatefulWidget {
   final List<CreateCSDto> cartState;
-  CartWidget({required this.cartState});
+  final VoidCallback onExit;
+  CartWidget({required this.cartState, required this.onExit});
 
   @override
   State<CartWidget> createState() => _CartWidgetState();
@@ -13,6 +14,32 @@ class CartWidget extends StatefulWidget {
 class _CartWidgetState extends State<CartWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Stack(children: <Widget>[
+      Padding(
+        padding: const EdgeInsets.fromLTRB(29.5, 40, 8, 0),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.95,
+          width: MediaQuery.of(context).size.width * 0.86,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+      Positioned(
+        top: 15,
+        left: -7,
+        child: IconButton(
+          onPressed: () {
+            widget.onExit();
+          },
+          icon: Icon(Icons.close_rounded),
+          color: Colors.black,
+          iconSize: 30,
+        ),
+      ),
+    ]);
   }
 }
