cb3eac29ca0482a7e4cea1d8a7815a7bc826a28a
Frans Billqvist <frans.billquist@gmail.com>
2024-01-30 07:55:05 +0100

    Added functionally to open cart, only accessable when Cart.isNotEmpty Page fully build on Stack insteed of Column

--- snusyoulooseflutter/lib/Pages/shop_page.dart
+++ snusyoulooseflutter/lib/Pages/shop_page.dart
@@ -24,6 +24,7 @@ class ShopPage extends StatefulWidget {
 
 class _ShopPageState extends State<ShopPage> {
   final shop = fetchSnuffShopService();
+  bool showCart = false;
 
   void updateCart(List<CreateCSDto> updatedCart) {
     setState(() {
@@ -40,114 +41,131 @@ class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: FutureBuilder<List<SnuffShopDto>>(
-        future: fetchSnuffShopService(),
-        builder: (context, snapshot) {
-          return Column(
-            children: [
-              Row(
-                children: [
-                  ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                        Colors.transparent,
-                      ),
-                      elevation: MaterialStateProperty.all(0),
-                    ),
-                    onPressed: () {
-                      Navigator.of(context)
-                          .pushReplacementNamed(AppRoutes.home);
-                    },
-                    child: Icon(
-                      Icons.arrow_back,
-                      size: 28,
-                      color: Colors.indigoAccent[200],
-                    ),
-                  ),
-                  Center(
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 24.0, left: 17),
-                      child: SizedBox(
-                        height: 64,
-                        width: MediaQuery.of(context).size.width * 0.61,
-                        child: Material(
-                          elevation:
-                              14.0, // Change this value to adjust the elevation
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.blue,
-                          child: Center(
-                            child: Text(
-                              "Add a Search-Function HERE",
-                              style: TextStyle(
-                                  color: AppColors.textOnFocus2, fontSize: 16),
+      body: SingleChildScrollView(
+        child: FutureBuilder<List<SnuffShopDto>>(
+          future: fetchSnuffShopService(),
+          builder: (context, snapshot) {
+            return Stack(
+              children: [
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                              Colors.transparent,
                             ),
+                            elevation: MaterialStateProperty.all(0),
                           ),
+                          onPressed: () {
+                            Navigator.of(context)
+                                .pushReplacementNamed(AppRoutes.home);
+                          },
+                          child: (!showCart
+                              ? Icon(
+                                  Icons.arrow_back,
+                                  size: 28,
+                                  color: Colors.indigoAccent[200],
+                                )
+                              : Padding(padding: EdgeInsets.only(left: 28))),
                         ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.only(top: 5.0),
-                      child: Stack(
-                        children: <Widget>[
-                          IconButton(
-                            icon: Icon(Icons.shopping_cart),
-                            color: (widget.numberOfItemsInCart <= 0
-                                ? Colors.white
-                                : Colors.indigoAccent[200]),
-                            onPressed: () {
-                              CartWidget(
-                                cartState: widget.itemsInMyCart,
-                              );
-                            },
-                          ),
-                          if (widget.numberOfItemsInCart > 0)
-                            Padding(
-                              padding:
-                                  const EdgeInsets.only(top: 8.0, left: 26),
-                              child: Positioned(
-                                right: 0,
-                                child: Container(
-                                  padding: EdgeInsets.all(1),
-                                  decoration: BoxDecoration(
-                                    color: Colors.red,
-                                    borderRadius: BorderRadius.circular(12),
-                                  ),
-                                  constraints: BoxConstraints(
-                                    minWidth: 12,
-                                    minHeight: 12,
-                                  ),
+                        Center(
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 24.0, left: 17),
+                            child: SizedBox(
+                              height: 64,
+                              width: MediaQuery.of(context).size.width * 0.61,
+                              child: Material(
+                                elevation: 14.0,
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.blue,
+                                child: Center(
                                   child: Text(
-                                    '${widget.numberOfItemsInCart}',
+                                    "Add a Search-Function HERE",
                                     style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 12,
-                                    ),
-                                    textAlign: TextAlign.center,
+                                        color: AppColors.textOnFocus2,
+                                        fontSize: 16),
                                   ),
                                 ),
                               ),
                             ),
-                        ],
-                      )),
-                ], //Row ends HERE:
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width,
-                child: ShopWidget(
-                  itemsInShopFuture: shop,
-                  onCartUpdated: (updatedCart) {
-                    updateCart(updatedCart);
-                  },
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 5.0),
+                          child: Stack(
+                            children: <Widget>[
+                              IconButton(
+                                icon: Icon(Icons.shopping_cart),
+                                color: (widget.numberOfItemsInCart <= 0
+                                    ? Colors.white
+                                    : Colors.indigoAccent[200]),
+                                onPressed: () {
+                                  toggleCart(widget.numberOfItemsInCart);
+                                },
+                              ),
+                              if (widget.numberOfItemsInCart > 0)
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.only(top: 8.0, left: 26),
+                                  child: Positioned(
+                                    right: 0,
+                                    child: Container(
+                                      padding: EdgeInsets.all(1),
+                                      decoration: BoxDecoration(
+                                        color: Colors.red,
+                                        borderRadius: BorderRadius.circular(12),
+                                      ),
+                                      constraints: BoxConstraints(
+                                        minWidth: 12,
+                                        minHeight: 12,
+                                      ),
+                                      child: Text(
+                                        '${widget.numberOfItemsInCart}',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 12,
+                                        ),
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height,
+                      width: MediaQuery.of(context).size.width,
+                      child: ShopWidget(
+                        itemsInShopFuture: shop,
+                        onCartUpdated: (updatedCart) {
+                          updateCart(updatedCart);
+                        },
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
-          );
-        },
+                if (showCart && widget.itemsInMyCart.isNotEmpty)
+                  CartWidget(
+                      cartState: widget.itemsInMyCart,
+                      onExit: () => toggleCart(widget.numberOfItemsInCart)),
+              ],
+            );
+          },
+        ),
       ),
-    ));
+    );
+  }
+
+  void toggleCart(int valueAboutZero) {
+    if (valueAboutZero > 0)
+      setState(() {
+        showCart = !showCart;
+      });
   }
 }
 
