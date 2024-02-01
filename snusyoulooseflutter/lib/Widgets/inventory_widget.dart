bc0324087fee0f6ce07e14bfd6ecf8449c51f2e8
Frans Billqvist <frans.billquist@gmail.com>
2024-02-01 10:55:12 +0100

    Add import for app_services.dart remove duplicated method

--- snusyoulooseflutter/lib/Widgets/inventory_widget.dart
+++ snusyoulooseflutter/lib/Widgets/inventory_widget.dart
@@ -4,6 +4,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:http/http.dart' as http;
+import 'package:snusyoulooseflutter/Services/app_services.dart';
 
 import '../Config/app_media.dart';
 import '../Config/app_strings.dart';
@@ -29,7 +30,7 @@ class InventoryWidget extends StatelessWidget {
         final userId = store.state.user!.UserId.toString();
         final amountLeft = currentSnuff.amountRemaing;
         return FutureBuilder<List<CurrentSnuffDto>>(
-          future: fetchUsersInventory(userId),
+          future: fetchUsersInventoryService(userId),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
               return CircularProgressIndicator(); // or some other placeholder
@@ -80,25 +81,4 @@ class InventoryWidget extends StatelessWidget {
       },
     );
   }
-}
-
-Future<List<CurrentSnuffDto>> fetchUsersInventory(String userId) async {
-  try {
-    final response = await http.get(
-        Uri.parse(AppUrls.fetchUsersInventory + userId),
-        headers: {'Content-Type': 'application/json'});
-    print('This is the fetchUsersInventory response: ${response.body} ');
-    if (response.statusCode == 200) {
-      final inventory = jsonDecode(response.body);
-      print('This is the inventory: $inventory');
-      inspect(inventory);
-      var result = List<CurrentSnuffDto>.from(
-          inventory.map((x) => CurrentSnuffDto.fromJson(x))).toList();
-      return result;
-      // return inventory as List<CurrentSnuff>;
-    }
-  } catch (err) {
-    print('FAILED TO FETCH USERS INVENTORY $err');
-  }
-  throw Exception('Failed to fetch users inventory');
 }
