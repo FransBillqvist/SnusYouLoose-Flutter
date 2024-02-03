af676bb6563feca125b139c783bf216c80a76d48
Frans Billqvist <frans.billquist@gmail.com>
2024-02-04 00:51:16 +0100

    Refactor home_page.dart might need to be reworked

--- snusyoulooseflutter/lib/Pages/home_page.dart
+++ snusyoulooseflutter/lib/Pages/home_page.dart
@@ -25,101 +25,115 @@ class _HomePageState extends State<HomePage> {
     final userid = getUserIdService(context);
     store.dispatch(getSnuffInventory(userid));
     store.dispatch(getUserHabit(userid, context));
-    store.dispatch(getPregression(userid));
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: StoreConnector<AppState, List<CurrentSnuffDto>>(
-          converter: (store) => store.state.inventorySnuffs,
-          builder: (context, List<CurrentSnuffDto> stateSnuff) => Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Stack(
-                children: [
-                  Positioned(
-                    top: 20,
-                    right: -24,
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 48.0),
-                      child: IconButton(
-                        icon: Icon(
-                          Icons.menu,
-                          color: Colors.black,
+    store.dispatch(getProgression(userid));
+
+    return StoreConnector<AppState, bool>(
+        converter: (store) => store.state.getPregressionFailed,
+        builder: (context, getPregressionFailed) {
+          if (getPregressionFailed) {
+            store.dispatch(createAPregression(userid));
+          }
+
+          return Scaffold(
+            body: SingleChildScrollView(
+              child: StoreConnector<AppState, List<CurrentSnuffDto>>(
+                converter: (store) => store.state.inventorySnuffs,
+                builder: (context, List<CurrentSnuffDto> stateSnuff) => Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Stack(
+                      children: [
+                        Positioned(
+                          top: 20,
+                          right: -24,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 48.0),
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.menu,
+                                color: Colors.black,
+                              ),
+                              onPressed: () {
+                                Scaffold.of(context).openEndDrawer();
+                              },
+                            ),
+                          ),
                         ),
-                        onPressed: () {
-                          Scaffold.of(context).openEndDrawer();
-                        },
-                      ),
+                        Positioned(
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.only(top: 530.0, left: 53),
+                            child: SizedBox(
+                                width: MediaQuery.of(context).size.width * 0.74,
+                                height:
+                                    MediaQuery.of(context).size.height * 0.4,
+                                child:
+                                    InventoryWidget(currentSnuffs: stateSnuff)),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            endDrawer: Drawer(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+                  DrawerHeader(
+                    child: Text(
+                      '${store.state.user?.FirstName}',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    decoration: BoxDecoration(
+                      color: AppColors.example2,
                     ),
                   ),
-                  Positioned(
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 530.0, left: 53),
-                      child: SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.74,
-                          height: MediaQuery.of(context).size.height * 0.4,
-                          child: InventoryWidget(currentSnuffs: stateSnuff)),
+                  ListTile(
+                    title: const Row(
+                      children: [
+                        Text(AppStrings.shop, style: TextStyle(fontSize: 22)),
+                        Padding(
+                          padding: EdgeInsets.only(left: 38),
+                          child: Icon(Icons.shopping_cart_rounded,
+                              size: 30, color: AppColors.cartBgLight),
+                        )
+                      ],
                     ),
+                    onTap: () {
+                      // Update the state of the app
+                      // Then close the drawer
+                      Navigator.of(context)
+                          .pushReplacementNamed(AppRoutes.shop);
+                    },
+                  ),
+                  ListTile(
+                    title: const Row(
+                      children: [
+                        Text(
+                          AppStrings.logout,
+                          style: TextStyle(fontSize: 22),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20),
+                          child: Icon(Icons.logout,
+                              size: 30, color: AppColors.cartBgLight),
+                        ),
+                      ],
+                    ),
+                    onTap: () {
+                      var current = store.state;
+                      Navigator.pop(context);
+                      store.dispatch(SignOutUserAction(current.user!));
+                      Navigator.of(context)
+                          .pushReplacementNamed(AppRoutes.login);
+                    },
                   ),
                 ],
               ),
-            ],
-          ),
-        ),
-      ),
-      endDrawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: Text(
-                '${store.state.user?.FirstName}',
-                style: TextStyle(fontSize: 18),
-              ),
-              decoration: BoxDecoration(
-                color: AppColors.example2,
-              ),
             ),
-            ListTile(
-              title: const Row(
-                children: [
-                  Text(AppStrings.shop, style: TextStyle(fontSize: 22)),
-                  Padding(
-                    padding: EdgeInsets.only(left: 38),
-                    child: Icon(Icons.shopping_cart_rounded,
-                        size: 30, color: AppColors.cartBgLight),
-                  )
-                ],
-              ),
-              onTap: () {
-                // Update the state of the app
-                // Then close the drawer
-                Navigator.of(context).pushReplacementNamed(AppRoutes.shop);
-              },
-            ),
-            ListTile(
-              title: const Row(
-                children: [
-                  Text(
-                    AppStrings.logout,
-                    style: TextStyle(fontSize: 22),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20),
-                    child: Icon(Icons.logout,
-                        size: 30, color: AppColors.cartBgLight),
-                  ),
-                ],
-              ),
-              onTap: () {
-                var current = store.state;
-                Navigator.pop(context);
-                store.dispatch(SignOutUserAction(current.user!));
-                Navigator.of(context).pushReplacementNamed(AppRoutes.login);
-              },
-            ),
-          ],
-        ),
-      ),
-    );
+          );
+        });
   }
 }
