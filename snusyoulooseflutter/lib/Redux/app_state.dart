063618f26c3e53b824e62e68f36efcd0fe9fab64
Frans Billqvist <frans.billquist@gmail.com>
2024-02-11 10:20:32 +0100

    Add fetchInventorySnuffs and fetchProgressionState methods

--- snusyoulooseflutter/lib/Redux/app_state.dart
+++ snusyoulooseflutter/lib/Redux/app_state.dart
@@ -1,11 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:snusyoulooseflutter/Model/HabitDto.dart';
 
 import '../Model/CurrentSnuffDto.dart';
 import '../Model/ProgressionDto.dart';
 import '../Model/Snuff.dart';
 import '../Model/User.dart';
+import '../Services/app_services.dart';
 
-class AppState {
+class AppState extends ChangeNotifier {
   List<CurrentSnuffDto> inventorySnuffs;
   List<Snuff> snuffsDetailsInInventory;
   Snuff? selectedSnuff;
@@ -23,4 +25,16 @@ class AppState {
     this.progressionState,
     this.getPregressionFailed = false,
   });
+
+  Future<void> fetchInventorySnuffs() async {
+    // Call your API here
+    inventorySnuffs = await fetchUsersInventoryService(user!.UserId!);
+    notifyListeners();
+  }
+
+  Future<void> fetchProgressionState() async {
+    // Call your API here
+    progressionState = await fetchUserProgressionService(user!.UserId!);
+    notifyListeners();
+  }
 }
