573f725b9d98fc4314168f1cf68629a39cf22064
Frans Billqvist <frans.billquist@gmail.com>
2024-02-19 00:46:54 +0100

    Add page selection functionality

--- snusyoulooseflutter/lib/Pages/statistics_page.dart
+++ snusyoulooseflutter/lib/Pages/statistics_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:snusyoulooseflutter/Model/StatisticDto.dart';
 
 import '../Config/app_routes.dart';
+import '../Config/app_strings.dart';
 import '../Services/app_services.dart';
 
 class StatisticsPage extends StatefulWidget {
@@ -12,26 +13,40 @@ class StatisticsPage extends StatefulWidget {
 }
 
 class _StatisticsPageState extends State<StatisticsPage> {
+  final List<String> _pages = [
+    AppStrings.todays,
+    AppStrings.week,
+    AppStrings.twoWeek,
+    AppStrings.month,
+    AppStrings.quarter,
+    AppStrings.year,
+    AppStrings.allTime
+  ];
+  int _selectedPageIndex = 0;
   @override
   Widget build(BuildContext context) {
     final userId = getUserIdService(context);
     return Scaffold(
       body: SingleChildScrollView(
-        child: FutureBuilder<StatisticDto>(
-          future: fetchDailyStatisticService(userId),
-          builder: (context, snapshot) {
-            return Stack(children: [
-              Positioned(
-                  top: 55,
-                  left: 125,
-                  child: ElevatedButton(
-                      onPressed: () => Navigator.of(context)
-                          .pushReplacementNamed(AppRoutes.home),
-                      child: Icon(Icons.home_filled))),
-              Positioned(top: 75, left: 145, child: Placeholder())
-            ]);
-          },
-        ),
+        child: Stack(children: [
+          Positioned(
+              top: 25,
+              left: 15,
+              child: ElevatedButton(
+                  onPressed: () => Navigator.of(context)
+                      .pushReplacementNamed(AppRoutes.home),
+                  child: Icon(Icons.home_filled))),
+          const Padding(
+            padding: const EdgeInsets.only(left: 16.0, top: 140),
+            child: SizedBox(
+                height: 500,
+                width: 380,
+                child: ColoredBox(
+                  color: Colors.blue,
+                )),
+          ),
+          Positioned(top: 140, left: 15, child: Placeholder())
+        ]),
       ),
     );
   }
