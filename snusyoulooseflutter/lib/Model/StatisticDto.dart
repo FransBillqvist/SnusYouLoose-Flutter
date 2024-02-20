ff52c26e0175675277f8a5bbb18108a9cd754930
Frans Billqvist <frans.billquist@gmail.com>
2024-02-20 03:28:05 +0100

    Update StatisticDto class to include numberOfDays

--- snusyoulooseflutter/lib/Model/StatisticDto.dart
+++ snusyoulooseflutter/lib/Model/StatisticDto.dart
@@ -6,21 +6,29 @@ class StatisticDto {
   List<int> usedAmountOfSnuffs;
   int totalAmoutUsed;
   int limitOfUse;
-  int rating;
+  double rating;
   String createdDate;
+  int? numberOfDays;
 
-  StatisticDto(this.userId, this.usedSnuffSorts, this.usedAmountOfSnuffs,
-      this.totalAmoutUsed, this.limitOfUse, this.rating, this.createdDate);
+  StatisticDto(
+      this.userId,
+      this.usedSnuffSorts,
+      this.usedAmountOfSnuffs,
+      this.totalAmoutUsed,
+      this.limitOfUse,
+      this.rating,
+      this.createdDate,
+      this.numberOfDays);
 
   factory StatisticDto.fromJson(Map<String, dynamic> json) => StatisticDto(
-        json['userId'] as String,
-        (json['usedSnuffSorts'] as List).map((e) => Snuff.fromJson(e)).toList(),
-        (json['usedAmountOfSnuffs'] as List).map((e) => e as int).toList(),
-        json['totalAmoutUsed'] as int,
-        json['limitOfUse'] as int,
-        json['rating'] as int,
-        json['createdDate'] as String,
-      );
+      json['userId'] as String,
+      (json['usedSnuffSorts'] as List).map((e) => Snuff.fromJson(e)).toList(),
+      (json['usedAmountOfSnuffs'] as List).map((e) => e as int).toList(),
+      json['totalAmoutUsed'] as int,
+      json['limitOfUse'] as int,
+      (json['rating'] as num).toDouble(),
+      json['createdDate'] as String,
+      json['numberOfDays'] as int?);
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
@@ -34,6 +42,7 @@ class StatisticDto {
     data['limitOfUse'] = this.limitOfUse;
     data['rating'] = this.rating;
     data['createdDate'] = this.createdDate;
+    data['numberOfDays'] = this.numberOfDays;
     return data;
   }
 }
