ad13de8d7cb9052707a18220672eb8c46c274700
Frans Billqvist <frans.billquist@gmail.com>
2024-03-01 01:37:38 +0100

    Refactor ConsumedWidget layout and add exit button WIP

--- snusyoulooseflutter/lib/Widgets/consumed_widget.dart
+++ snusyoulooseflutter/lib/Widgets/consumed_widget.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:snusyoulooseflutter/Styles/app_colors.dart';
 
+import '../Config/app_media.dart';
 import '../Model/Snuff.dart';
 
 class ConsumedWidget extends StatefulWidget {
-  List<Snuff> snuffs;
-  List<int> amounts;
+  final List<Snuff> snuffs;
+  final List<int> amounts;
   final VoidCallback onExit;
   ConsumedWidget(
       {required this.snuffs, required this.amounts, required this.onExit});
@@ -18,14 +19,23 @@ class _ConsumedWidgetState extends State<ConsumedWidget> {
   @override
   Widget build(BuildContext context) {
     print("CONSUMED WIDGET");
-    return Stack(
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.fromLTRB(29.5, 40, 8, 0),
-          child: Column(
-            children: [
+    return SingleChildScrollView(
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            child: IconButton(
+              onPressed: widget.onExit,
+              icon: Icon(Icons.arrow_back),
+              iconSize: 40,
+              color: AppColors.example2,
+            ),
+            top: 20,
+            left: 20,
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(29.5, 40, 8, 0),
+            child: Column(children: [
               Container(
-                height: 92.0 * widget.snuffs.length,
                 width: MediaQuery.of(context).size.width * 0.86,
                 decoration: BoxDecoration(
                   color: AppColors.example2,
@@ -39,69 +49,60 @@ class _ConsumedWidgetState extends State<ConsumedWidget> {
                     ),
                   ],
                 ),
-                child: ListView.builder(
-                  itemCount: widget.snuffs.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                      child: Row(
-                        children: [
-                          Container(
-                            height: 70,
-                            width: 70,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              image: DecorationImage(
-                                image:
-                                    AssetImage(widget.snuffs[index].ImageUrl),
-                                fit: BoxFit.cover,
+                child: Column(
+                  children: List.generate(
+                    widget.snuffs.length,
+                    (index) {
+                      return Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                        child: Row(
+                          children: [
+                            Container(
+                              height: 70,
+                              width: 70,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage(
+                                    AppSnuffs.images[
+                                        widget.snuffs[index].ImageUrl!]!,
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
                               ),
                             ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  widget.snuffs[index].Brand,
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    widget.snuffs[index].Brand,
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
-                                ),
-                                Text(
-                                  'Amount: ${widget.amounts[index]}',
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    color: Colors.grey,
+                                  Text(
+                                    'Amount: ${widget.amounts[index]}',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.grey,
+                                    ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
+                          ],
+                        ),
+                      );
+                    },
+                  ),
                 ),
-              )
-            ],
-          ),
-        ),
-        Positioned(
-          top: 15,
-          left: -7,
-          child: IconButton(
-            onPressed: () {
-              widget.onExit();
-            },
-            icon: Icon(Icons.close_rounded),
-            color: Colors.black,
-            iconSize: 30,
-          ),
-        ),
-      ],
+              ),
+            ]),
+          )
+        ],
+      ),
     );
   }
 }
