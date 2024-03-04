48ed4059292f63d2fa3835999da3ad4ba361c139
Frans Billqvist <frans.billquist@gmail.com>
2024-03-04 03:59:27 +0100

    Add format conversion functions and map for Format enum

--- snusyoulooseflutter/lib/Model/Enums/Format.dart
+++ snusyoulooseflutter/lib/Model/Enums/Format.dart
@@ -5,3 +5,18 @@ enum Format {
   mini,
   loose,
 }
+
+final formatValues = Map<int, Format>.fromIterable(Format.values,
+    key: (v) => v.index, value: (v) => v);
+
+Format? formatFromInt(int number) {
+  if (formatValues.containsKey(number)) {
+    return formatValues[number];
+  } else {
+    throw ArgumentError('Invalid format number: $number');
+  }
+}
+
+int intFromFormat(Format format) {
+  return format.index;
+}
