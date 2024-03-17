73edcb02ac63cb0a535b2664909fb5cc378b9bb2
Frans Billqvist <frans.billquist@gmail.com>
2024-03-17 03:58:00 +0100

    Add NicotineUsageIndicatorWidget to display nicotine usage indicator WIP!!

---
+++ snusyoulooseflutter/lib/Widgets/nicotineusageindicator_widget.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+import '../Model/NicotineStats.dart';
+import '../Services/app_services.dart';
+
+class NicotineUsageIndicatorWidget extends StatelessWidget {
+  final String userId;
+  final String period;
+
+  NicotineUsageIndicatorWidget({required this.userId, required this.period});
+
+  @override
+  Widget build(BuildContext context) {
+    Stream<NicotineStats>? nicotineStatsStream =
+        fetchNicotineStatsStream(userId, period) as Stream<NicotineStats>?;
+    return StreamBuilder<NicotineStats>(
+      stream: nicotineStatsStream,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          // Show a loading indicator while fetching data
+          return CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          // Show an error message if there's an error
+          return Text('Error: ${snapshot.error}');
+        } else {
+          // Calculate rotation angle outside the return statement
+          double rotationAngle = 0;
+          if (snapshot.data?.indicationScore != null) {
+            double indicationScore = snapshot.data!.indicationScore;
+            if (indicationScore < 1) {
+              rotationAngle = -90 * (1 - indicationScore);
+            } else if (indicationScore > 1) {
+              rotationAngle = 90 * (indicationScore - 1);
+            }
+          }
+
+          // Display the nicotine usage indicator widget
+          return Container(
+            width: 100,
+            height: 100,
+            child: Stack(
+              children: [
+                // Create the arrow widget with rotation
+                Positioned(
+                  left: 0,
+                  top: 0,
+                  right: 0,
+                  bottom: 0,
+                  child: Transform.rotate(
+                    angle: rotationAngle, // Use the previously calculated angle
+                    child: Icon(
+                      Icons.arrow_forward,
+                      size: 50,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
+      },
+    );
+  }
+}
