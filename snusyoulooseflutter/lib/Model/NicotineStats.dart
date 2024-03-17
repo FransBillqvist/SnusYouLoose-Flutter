8a15f2ac8f1929584e689fee85d851bc97d281b3
Frans Billqvist <frans.billquist@gmail.com>
2024-03-17 03:57:37 +0100

    Fix typo in NicotineStats class

--- snusyoulooseflutter/lib/Model/NicotineStats.dart
+++ snusyoulooseflutter/lib/Model/NicotineStats.dart
@@ -3,20 +3,20 @@ class NicotineStats {
   int primaryNumberOfDays;
   double secondaryNicotineUse;
   int secondaryNumberOfDays;
-  double indecationScore;
+  double indicationScore;
 
   NicotineStats(
       this.primaryNicotineUse,
       this.primaryNumberOfDays,
       this.secondaryNicotineUse,
       this.secondaryNumberOfDays,
-      this.indecationScore);
+      this.indicationScore);
 
   factory NicotineStats.fromJson(Map<String, dynamic> json) => NicotineStats(
         (json['primaryNicotineUse'] as num).toDouble(),
         json['primaryNumberOfDays'] as int,
         (json['secondaryNicotineUse'] as num).toDouble(),
         json['secondaryNumberOfDays'] as int,
-        (json['indecationScore'] as num).toDouble(),
+        (json['indicationScore'] as num).toDouble(),
       );
 }
